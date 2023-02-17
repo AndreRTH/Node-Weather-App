aacf6d3d2d0ff979650f5ec4c340095ffd1162b9
Andres Barreras <andresbarreras@Administrators-MacBook-Pro-2.local>
2023-02-17 12:45:16 -0500

    Location no longer hardcoded

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const geocode = require('./utils/geocode.js');
 const forecast = require('./utils/forecast.js');
 
+const location = process.argv[2];
 
 
 
-geocode('Houston', (error, data) => {
-    console.log('Error', error);
-    console.log('data', data);
 
-    forecast(data.latitude, data.longitude, (error, data) => {
-        console.log('Error', error)
-        console.log('Data', data)
+geocode(location, (error, data) => {
+    if (error){
+        return console.log(error);
+    }
+
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+        if (error){
+            return console.log(error);
+        }
+        console.log(data.location);
+        console.log(forecastData);
       })
     
 
